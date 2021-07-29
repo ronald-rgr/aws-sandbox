eaf04bc320e8fd87fa53eedd119b05ec8a1a8ff4
Derek Osaduik <dosaduik@gmail.com>
2021-07-29 14:35:49 -0600

    Initial cut of helloworld. Haven't added any other nifty frameworks yet

---
+++ src/helloworld/helloworld-api/Controllers/HelloWorldController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace helloworld_api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HelloWorldController : ControllerBase
+    {
+        private readonly ILogger<HelloWorldController> _logger;
+
+        public HelloWorldController(ILogger<HelloWorldController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("SayHello")]
+        public string SayHello()
+        {
+            return "Hello World!";
+        }
+    }
+}
